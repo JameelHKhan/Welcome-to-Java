40fa255d58edf83e6cd1601c100ce3db9905f416
Jameel Khan <jkhan11@jh.edu>
2021-02-24 15:23:52 -0500

    Add files via upload

---
+++ WeekOneAssignment.java
@@ -0,0 +1,17 @@
+/* 
+ * Jameel H. Khan 
+ * Module 1 Assignment
+ * EN.605.201.83.SP21 
+ */ 
+
+public class WeekOneAssignment
+{
+	public static void main( String[] args )
+	{
+		System.out.println();
+		System.out.println("    J    A    V     V    A");
+		System.out.println("    J   A A    V   V    A A");
+		System.out.println("J   J  AAAAA    V V    AAAAA");
+		System.out.println(" J J  A     A    V    A     A");
+	} // end of main method
+} // end of class WeekOneAssignment
